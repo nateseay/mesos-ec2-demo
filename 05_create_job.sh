4bdfd08867fe1664cb01be12b82c80a636ddaea1
Nate Seay <nate.seay@gmail.com>
2016-05-16 22:01:34 -0700

    Cleanup

--- 05_create_job.sh
+++ 05_create_job.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
- curl -X POST -d @test_app.json http://${1}:8080/v2/apps  --header "Content-Type:application/json"
+ curl -X POST -d @test_app.json http://${1}:8080/v2/apps  --header "Content-Type:application/json" | python -m json.tool
